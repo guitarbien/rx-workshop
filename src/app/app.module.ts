537977bcda8796eb0b4cb96d13bceb6cfc1d3263
guitarbien <guitarbien@gmail.com>
2018-01-02 18:06:26 +0800

    載入主要 module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import {IPostService} from './interface/ipost-service.interface';
+import {PostService} from './service/post/post.service';
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {provide: IPostService, useClass: PostService}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
