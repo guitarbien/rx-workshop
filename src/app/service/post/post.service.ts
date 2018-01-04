a04ecc3d93078c07df07b3ee8c95b6c15202c331
guitarbien <guitarbien@gmail.com>
2018-01-04 11:01:46 +0800

    implements interfaces

--- src/app/service/post/post.service.ts
+++ src/app/service/post/post.service.ts
@@ -3,9 +3,16 @@ import {IPostService} from '../../interface/ipost-service.interface';
 import {Post} from '../../model/post.model';
 import {Observable} from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
+import { IAddPost } from '../../interface/iadd-post.interface';
+import { IListPosts } from '../../interface/ilist-posts.interface';
+import { IShowCount } from '../../interface/ishow-count.interface';
+import { IReloadPosts } from '../../interface/ireload-posts.interface';
 
 @Injectable()
-export class PostService implements IPostService {
+export class PostService implements IAddPost, IListPosts, IShowCount, IReloadPosts {
+
+  posts$: Observable<Post[]>;
+
   constructor(private httpClient: HttpClient) { }
 
   addPost(post: Post): void {
@@ -16,5 +23,8 @@ export class PostService implements IPostService {
 
   getPosts(): Observable<Post[]> {
     return this.httpClient.get<Post[]>('api/posts');
+  }
+
+  reloadPosts(): void {
   }
 }
