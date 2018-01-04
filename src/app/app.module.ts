20b0ed83e1b0b079b2629793ea9c5dbfff2eacef
guitarbien <guitarbien@gmail.com>
2018-01-04 11:47:10 +0800

    DI 使用 singleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import {IPostService} from './interface/ipost-service.interface';
 import {PostService} from './service/post/post.service';
 import {FormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -10,6 +8,10 @@ import { AddPostComponent } from './component/add-post/add-post.component';
 import { ListPostComponent } from './component/list-post/list-post.component';
 import { ReloadPostsComponent } from './component/reload-posts/reload-posts.component';
 import { ShowCountComponent } from './component/show-count/show-count.component';
+import { IAddPost } from './interface/iadd-post.interface';
+import { IListPosts } from './interface/ilist-posts.interface';
+import { IShowCount } from './interface/ishow-count.interface';
+import { IReloadPosts } from './interface/ireload-posts.interface';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,11 @@ import { ShowCountComponent } from './component/show-count/show-count.component'
     HttpClientModule
   ],
   providers: [
-    {provide: IPostService, useClass: PostService}
+    PostService,
+    {provide: IAddPost, useExisting: PostService},
+    {provide: IListPosts, useExisting: PostService},
+    {provide: IShowCount, useExisting: PostService},
+    {provide: IReloadPosts, useExisting: PostService}
   ],
   bootstrap: [AppComponent]
 })
