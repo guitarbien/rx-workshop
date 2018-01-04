826656d4a58e029eccd276235016a4d0094449f7
guitarbien <guitarbien@gmail.com>
2018-01-04 11:46:57 +0800

    使用 store$ 控管內部使用的資料

--- src/app/service/post/post.service.ts
+++ src/app/service/post/post.service.ts
@@ -1,30 +1,34 @@
 import { Injectable } from '@angular/core';
-import {IPostService} from '../../interface/ipost-service.interface';
-import {Post} from '../../model/post.model';
-import {Observable} from 'rxjs/Observable';
+import { Post } from '../../model/post.model';
+import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
 import { IAddPost } from '../../interface/iadd-post.interface';
 import { IListPosts } from '../../interface/ilist-posts.interface';
 import { IShowCount } from '../../interface/ishow-count.interface';
 import { IReloadPosts } from '../../interface/ireload-posts.interface';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class PostService implements IAddPost, IListPosts, IShowCount, IReloadPosts {
-
-  posts$: Observable<Post[]>;
+  private store$: BehaviorSubject<Post[]> = new BehaviorSubject<Post[]>([]);
+  // readonly posts$: Observable<Post[]> = this.store$.asObservable();
+  readonly posts$: Observable<Post[]> = this.store$;
 
   constructor(private httpClient: HttpClient) { }
 
   addPost(post: Post): void {
+    // 對 api 新增資料
     this.httpClient
         .post<Post>('api/posts', post)
         .subscribe();
-  }
 
-  getPosts(): Observable<Post[]> {
-    return this.httpClient.get<Post[]>('api/posts');
+    // 對給內部共用的變數新增資料
+    this.store$.next([...this.store$.getValue(), post]);
   }
 
   reloadPosts(): void {
+    this.httpClient
+      .get<Post[]>('api/posts')
+      .subscribe(posts => this.store$.next(posts));
   }
 }
