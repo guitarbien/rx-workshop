f905a8936f113f88b295910482327c9cb9dfeb02
guitarbien <guitarbien@gmail.com>
2018-01-04 10:21:19 +0800

    (delete unused code)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import 'rxjs/add/observable/from';
-import 'rxjs/add/observable/zip';
+// import 'rxjs/add/observable/from';
+// import 'rxjs/add/observable/zip';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +8,6 @@ import 'rxjs/add/observable/zip';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // onClick() {
-  //   const a$: Observable<number> = Observable.from([1, 3, 5]);
-  //   const b$: Observable<number> = Observable.from([2, 4, 6]);
-  //
-  //   const sum$: Observable<number> = Observable.zip(a$, b$, (a, b) => a + b);
-  //
-  //   sum$.subscribe(sum => console.log(sum));
-  // }
 
   constructor() {
   }
