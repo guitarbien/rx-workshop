54051b929323d7447d5951b2018ede495890c0d7
guitarbien <guitarbien@gmail.com>
2018-01-11 23:20:08 +0800

    設置根目錄 route：/counter、/post

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CounterComponent } from './component/counter/counter.component';
+import { IndexPostComponent } from './component/index-post/index-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'post', pathMatch: 'full'},
+  {path: 'counter', component: CounterComponent},
+  {path: 'post/:id', component: IndexPostComponent},
+  {path: 'post', component: IndexPostComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
